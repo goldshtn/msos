dcab61f27e7427966250ff565fe640145062708f
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-07 21:21:45 +0300

    Added forgotten files

---
+++ msos/HeapQuery.cs
@@ -0,0 +1,90 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msos
+{
+    [Verb("!hq", HelpText = "TODO")]
+    class HeapQuery : ICommand
+    {
+        [Value(0, Required = true)]
+        public string Query { get; set; }
+
+        private class QueryOutputWriter : TextWriter
+        {
+            private CommandExecutionContext _context;
+            private StringBuilder _buffer = new StringBuilder();
+
+            public QueryOutputWriter(CommandExecutionContext context)
+            {
+                _context = context;
+            }
+
+            public override void Write(char value)
+            {
+                _buffer.Append(value);
+                if (value == '\n')
+                {
+                    _context.WriteLine(_buffer.ToString().TrimEnd('\r', '\n'));
+                    _buffer.Clear();
+                }
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (_buffer.Length != 0)
+                {
+                    _context.WriteLine(_buffer.ToString());
+                    _buffer.Clear();
+                }
+                base.Dispose(disposing);
+            }
+
+            public override Encoding Encoding
+            {
+                get { return Encoding.UTF8; }
+            }
+        }
+
+        public void Execute(CommandExecutionContext context)
+        {
+            AppDomain appDomain = AppDomain.CreateDomain("RunQueryAppDomain");
+
+            using (QueryOutputWriter writer = new QueryOutputWriter(context))
+            {
+                object[] arguments;
+                if (context.ProcessId != 0)
+                {
+                    arguments = new object[] { context.ProcessId, context.DacLocation, writer };
+                }
+                else
+                {
+                    arguments = new object[] { context.DumpFile, context.DacLocation, writer };
+                }
+                using (RunInSeparateAppDomain runner = (RunInSeparateAppDomain)appDomain.CreateInstanceAndUnwrap(
+                    typeof(RunInSeparateAppDomain).Assembly.FullName,
+                    typeof(RunInSeparateAppDomain).FullName,
+                    false, System.Reflection.BindingFlags.CreateInstance, null,
+                    arguments, null, null
+                    )
+                )
+                {
+                    try
+                    {
+                        runner.RunQuery(Query);
+                    }
+                    catch (RunFailedException ex)
+                    {
+                        context.WriteError(ex.Message);
+                    }
+                }
+            }
+
+            AppDomain.Unload(appDomain);
+        }
+    }
+}
---
+++ msos/RunInSeparateAppDomain.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.CSharp;
+using Microsoft.Diagnostics.Runtime;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo(msos.RunInSeparateAppDomain.CompiledQueryAssemblyName)]
+
+namespace msos
+{
+    internal class RunQueryContext
+    {
+        public ClrHeap Heap { get; set; }
+    }
+
+    internal interface IRunQuery
+    {
+        object Run();
+    }
+
+    [Serializable]
+    public class RunFailedException : Exception
+    {
+        public RunFailedException(string message)
+            : base(message)
+        {
+        }
+
+        protected RunFailedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+
+    class RunInSeparateAppDomain : MarshalByRefObject, IDisposable
+    {
+        internal const string CompiledQueryAssemblyName = "msos_CompiledQuery";
+        const int AttachTimeout = 1000;
+        const string CompiledQueryPlaceholder = "$$$QUERY$$$";
+        const string CompiledQueryTemplate = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using msos;
+
+[assembly: InternalsVisibleTo(""msos"")]
+
+internal class RunQuery : IRunQuery
+{
+    private RunQueryContext _context;
+
+    public RunQuery(RunQueryContext context)
+    {
+        _context = context;
+    }
+
+    private IEnumerable<ulong> AllObjects()
+    {
+        return _context.Heap.EnumerateObjects();
+    }
+
+    public object Run()
+    {
+        return " + CompiledQueryPlaceholder + @";
+    }
+}
+";
+
+        private TextWriter _writer;
+        private ClrHeap _heap;
+        private DataTarget _target;
+
+        private void CreateRuntime(string dacLocation)
+        {
+            ClrRuntime runtime = _target.CreateRuntime(dacLocation);
+            _heap = runtime.GetHeap();
+        }
+
+        public RunInSeparateAppDomain(string dumpFile, string dacLocation, TextWriter writer)
+        {
+            _target = DataTarget.LoadCrashDump(dumpFile, CrashDumpReader.ClrMD);
+            CreateRuntime(dacLocation);
+            _writer = writer;
+        }
+
+        public RunInSeparateAppDomain(int pid, string dacLocation, TextWriter writer)
+        {
+            _target = DataTarget.AttachToProcess(pid, AttachTimeout, AttachFlag.Passive);
+            CreateRuntime(dacLocation);
+            _writer = writer;
+        }
+
+        public void RunQuery(string query)
+        {
+            var options = new CompilerParameters();
+            options.ReferencedAssemblies.Add(typeof(Enumerable).Assembly.Location);
+            options.ReferencedAssemblies.Add(Assembly.GetExecutingAssembly().Location);
+            options.ReferencedAssemblies.Add(typeof(ClrHeap).Assembly.Location);
+            options.CompilerOptions = "/optimize+";
+            options.OutputAssembly = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                CompiledQueryAssemblyName) + ".dll";
+
+            string source = CompiledQueryTemplate.Replace(CompiledQueryPlaceholder, query);
+
+            var compiler = new CSharpCodeProvider();
+            CompilerResults results = compiler.CompileAssemblyFromSource(options, source);
+
+            if (results.Errors.HasErrors)
+            {
+                throw new RunFailedException(
+                    String.Format("Query compilation failed with {0} errors:" + Environment.NewLine + "{1}",
+                    results.Errors.Count,
+                    String.Join(Environment.NewLine, (from error in results.Errors.Cast<CompilerError>() select error.ToString()).ToArray())
+                    ));
+            }
+
+            Type compiledQueryType = results.CompiledAssembly.GetType("RunQuery");
+            IRunQuery runQuery = (IRunQuery)Activator.CreateInstance(
+                compiledQueryType, new RunQueryContext { Heap = _heap });
+            
+            object result = runQuery.Run();
+            IEnumerable enumerable = result as IEnumerable;
+            ulong rowCount = 0;
+            if (enumerable != null)
+            {
+                foreach (var obj in enumerable)
+                {
+                    if (obj is ulong)
+                    {
+                        _writer.WriteLine(((ulong)obj).ToString("x16"));
+                    }
+                    else
+                    {
+                        _writer.WriteLine(obj.ToString());
+                    }
+                    ++rowCount;
+                }
+            }
+            else
+            {
+                _writer.WriteLine(result.ToString());
+                ++rowCount;
+            }
+            _writer.WriteLine("Rows: {0}", rowCount);
+        }
+
+        public void Dispose()
+        {
+            _target.Dispose();
+        }
+    }
+}
