6f4bcb0d370b88b2d5955ac979733dc545eb538c
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-13 18:59:42 +0300

    Added CPU utilization to --diag output

--- msos/TimeAndMemory.cs
+++ msos/TimeAndMemory.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace msos
 {
+    using FileTime = System.Runtime.InteropServices.ComTypes.FILETIME;
+
     class TimeAndMemory : IDisposable
     {
         private Stopwatch _sw;
         private long _heapSizeStart;
         private bool _diagnosticModeOn;
         private IPrinter _printer;
+        private ulong _cpuTimeBegin;
 
         private static void DoFullCollect()
         {
@@ -28,6 +32,7 @@ namespace msos
             if (!_diagnosticModeOn)
                 return;
 
+            _cpuTimeBegin = GetCpuTimeTicks();
             _sw = Stopwatch.StartNew();
             DoFullCollect();
             _heapSizeStart = GC.GetTotalMemory(false);
@@ -38,15 +43,36 @@ namespace msos
             if (!_diagnosticModeOn)
                 return;
 
+            ulong cpuTimeEnd = GetCpuTimeTicks();
             _sw.Stop();
+
             DoFullCollect();
             long heapSizeEnd = GC.GetTotalMemory(false);
-            _printer.WriteInfo("Time: {0} ms, Memory start: {1}, Memory end: {2}, Memory delta: {3}{4}",
-                _sw.ElapsedMilliseconds,
+
+            var wallClockElapsed = _sw.Elapsed.TotalMilliseconds;
+            var cpuElapsed = new TimeSpan((long)(cpuTimeEnd - _cpuTimeBegin)).TotalMilliseconds;
+
+            _printer.WriteInfo(
+                "Time: {0:N} ms, CPU time: {1:N} ms ({2:N}%)" + Environment.NewLine +
+                "Memory start: {3}, Memory end: {4}, Memory delta: {5}{6}",
+                wallClockElapsed, cpuElapsed, 100.0*cpuElapsed/wallClockElapsed,
                 ((ulong)_heapSizeStart).ToMemoryUnits(),
                 ((ulong)heapSizeEnd).ToMemoryUnits(), 
                 heapSizeEnd > _heapSizeStart ? "+" : "-",
                 ((ulong)Math.Abs(heapSizeEnd - _heapSizeStart)).ToMemoryUnits());
         }
+
+        private static ulong GetCpuTimeTicks()
+        {
+            FileTime creationTime, exitTime, kernelTime, userTime;
+            if (!GetProcessTimes((IntPtr)(-1), out creationTime, out exitTime, out kernelTime, out userTime))
+                return 0;
+
+            return (((ulong)userTime.dwHighDateTime) << 32) + (ulong)userTime.dwLowDateTime;
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern bool GetProcessTimes(IntPtr hProcess, out FileTime lpCreationTime,
+            out FileTime lpExitTime, out FileTime lpKernelTime, out FileTime lpUserTime);
     }
 }
