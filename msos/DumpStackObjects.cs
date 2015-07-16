8e4ee6dfd98b0bc4c19f3044ec293ad04092c521
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-16 13:43:58 +0300

    Added DumpStackObjects.cs (forgot in previous commit)

---
+++ msos/DumpStackObjects.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msos
+{
+    [Verb("!dso", HelpText =
+        "Dump stack objects for the specified thread. Not all objects displayed are actually " +
+        "referenced by live local variables.")]
+    class DumpStackObjects : ICommand
+    {
+        public void Execute(CommandExecutionContext context)
+        {
+            context.WriteLine("{0,-20} {1,-20} {2}", "Address", "Object", "Type");
+            foreach (var localRoot in context.CurrentThread.EnumerateStackObjects())
+            {
+                var type = context.Heap.GetObjectType(localRoot.Object);
+
+                context.Write("{0,-20:x16} {1,-20:x16} {2} ", localRoot.Address, localRoot.Object,
+                    type == null ? "" : type.Name);
+                if (type != null && !type.IsFree)
+                {
+                    context.WriteLink("", String.Format("!do {0:x16}", localRoot.Object));
+                }
+                context.WriteLine();
+            }
+        }
+    }
+}
