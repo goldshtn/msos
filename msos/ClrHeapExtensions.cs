9e0cb1fd18f38ac482268a3fc38d30bdc09a2167
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-23 13:53:14 +0300

    Added --flat mode to !objsize

--- msos/ClrHeapExtensions.cs
+++ msos/ClrHeapExtensions.cs
@@ -9,6 +9,62 @@ namespace msos
 {
     static class ClrHeapExtensions
     {
+        /// <summary>
+        /// Returns only referenced objects that do not contain further pointers,
+        /// and referenced arrays of objects that do not contain further pointers.
+        /// This provides a fast estimate of the amount of memory that is likely 
+        /// only retained by the target object.
+        /// </summary>
+        public static IEnumerable<Tuple<ulong, ClrType>> FlatSubgraphOf(this ClrHeap heap, ulong objPtr)
+        {
+            var type = heap.GetObjectType(objPtr);
+            if (type == null || type.IsFree || String.IsNullOrEmpty(type.Name))
+                yield break;
+
+            yield return new Tuple<ulong, ClrType>(objPtr, type);
+
+            List<Tuple<ulong, ClrType>> results = new List<Tuple<ulong, ClrType>>();
+            type.EnumerateRefsOfObject(objPtr, (inner, offset) =>
+            {
+                var innerType = heap.GetObjectType(inner);
+
+                if (innerType == null || innerType.IsFree)
+                {
+                    return;
+                }
+                if (innerType.IsArray &&
+                    (innerType.ArrayComponentType == null || innerType.ArrayComponentType.ContainsPointers))
+                {
+                    return;
+                }
+                if (!innerType.IsArray && innerType.ContainsPointers)
+                {
+                    return;
+                }
+
+                results.Add(new Tuple<ulong, ClrType>(inner, innerType));
+                
+                // Value type instances are sized along with the containing array.
+                if (innerType.IsArray && innerType.ElementType != ClrElementType.Struct)
+                {
+                    var arraySize = innerType.GetArrayLength(inner);
+                    for (int i = 0; i < arraySize; ++i)
+                    {
+                        var address = innerType.GetArrayElementAddress(inner, i);
+                        ulong value;
+                        if (heap.ReadPointer(address, out value))
+                        {
+                            results.Add(new Tuple<ulong, ClrType>(
+                                value, innerType.ArrayComponentType));
+                        }
+                    }
+                }
+            });
+
+            foreach (var result in results)
+                yield return result;
+        }
+
         public static IEnumerable<Tuple<ulong, ClrType>> SubgraphOf(this ClrHeap heap, ulong objPtr)
         {
             return EnumerateFromObjectSet(heap, new[] { objPtr });
--- msos/ObjSize.cs
+++ msos/ObjSize.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using Microsoft.Diagnostics.Runtime;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -8,11 +9,14 @@ using System.Threading.Tasks;
 
 namespace msos
 {
-    [Verb("!ObjSize", HelpText = "Display the size of the object graph referenced by the specified object, and including it.")]
+    [Verb("!ObjSize", HelpText = "Displays the size of the object graph referenced by the specified object, and including it.")]
     class ObjSize : ICommand
     {
         [Value(0, Required = true)]
         public string ObjectAddress { get; set; }
+
+        [Option("flat", HelpText = "Counts only referenced objects that do not contain further references.")]
+        public bool Flat { get; set; }
 
         public void Execute(CommandExecutionContext context)
         {
@@ -20,14 +24,25 @@ namespace msos
             if (!CommandHelpers.ParseAndVerifyValidObjectAddress(context, ObjectAddress, out objPtr))
                 return;
 
+            IEnumerable<Tuple<ulong, ClrType>> subgraph;
+            if (Flat)
+            {
+                subgraph = context.Heap.FlatSubgraphOf(objPtr);
+            }
+            else
+            {
+                subgraph = context.Heap.SubgraphOf(objPtr);
+            }
+
             ulong count = 0, size = 0;
-            foreach (var objAndType in context.Heap.SubgraphOf(objPtr))
+            foreach (var objAndType in subgraph)
             {
                 ++count;
                 size += objAndType.Item2.GetSize(objAndType.Item1);
             }
 
-            context.WriteLine("{0:x16} graph size is {1} objects, {2} bytes", objPtr, count, size);
+            context.WriteLine("{0:x16} graph size is {1} objects, {2} bytes ({3})",
+                objPtr, count, size, size.ToMemoryUnits());
         }
     }
 }
