3011e97f9f73f362c13a8e6c9fa0bb212f453022
Pavel-Durov <pdurov0@gmail.com>
2017-01-27 15:14:11 +0200

    Rading NtDelayExecution parameters and seting as blocking object (same as with CriticalSections)

--- msos/NativeStructs.cs
+++ msos/NativeStructs.cs
@@ -51,17 +51,17 @@ namespace msos
 
         #region NtDll
 
-
-        //[StructLayout(LayoutKind.Explicit, Size = 8)]
+        [StructLayout(LayoutKind.Explicit, Size = 8)]
         public struct LARGE_INTEGER
         {
-            //[FieldOffset(0)]
-            //public Int64 QuadPart;
-            //[FieldOffset(0)]
-            //public UInt32 LowPart;
-            //[FieldOffset(4)]
-            //public Int32 HighPart;
+            [FieldOffset(0)]
             public Int64 QuadPart;
+            [FieldOffset(0)]
+            public UInt32 LowPart;
+            [FieldOffset(4)]
+            public Int32 HighPart;
+        }
+
             public Int32 HighPart;
         }
 
